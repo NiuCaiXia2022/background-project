2f365cd26c7774e4fe9c02c08b88e948a4d0feec
niucaixia <312387661@qq.com>
2022-06-26 15:33:28 +0800

    feat(element-plus): 添加了element-plus+修改template标签报错

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
+// 安装element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus).use(store).use(router).mount('#app')
---
+++ src/views/login/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="login-content">登录</div>
+</template>
+<script setup>
+
+</script>
+<style lang="scss" scoped>
+</style>
